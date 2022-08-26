02d4dc860c25a8da455c455635f5d4f360820df1
cmancode <ing.mbcarlosa@gmail.com>
2022-08-26 15:44:22 -0500

    First commit

---
+++ gradebook-web/src/main/java/com/liferay/traning/gradebook/web/constants/GradebookPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.traning.gradebook.web.constants;
+
+/**
+ * @author Usuario
+ */
+public class GradebookPortletKeys {
+
+	public static final String GRADEBOOK =
+		"com_liferay_traning_gradebook_web_portlet_GradebookPortlet";
+
+}
---
+++ gradebook-web/src/main/java/com/liferay/traning/gradebook/web/portlet/GradebookPortlet.java
@@ -0,0 +1,32 @@
+package com.liferay.traning.gradebook.web.portlet;
+
+import com.liferay.traning.gradebook.web.constants.GradebookPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Usuario
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Gradebook",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + GradebookPortletKeys.GRADEBOOK,
+		 "com.liferay.portlet.display-category=category.training",
+		 "com.liferay.portlet.instanceable=false",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class GradebookPortlet extends MVCPortlet {
+}
